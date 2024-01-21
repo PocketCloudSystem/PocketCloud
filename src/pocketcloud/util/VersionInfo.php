18f32da10f02326ebb8daa60a0959e304af05365
r3pt1s <leonistmegacool2@gmail.com>
2024-01-21 19:00:39 +0100

    changed version

--- src/pocketcloud/util/VersionInfo.php
+++ src/pocketcloud/util/VersionInfo.php
@@ -4,7 +4,7 @@ namespace pocketcloud\util;
 
 final class VersionInfo {
 
-    public const VERSION = "2.1.5";
+    public const VERSION = "2.1.6";
     public const DEVELOPERS = ["r3pt1s"];
     public const BETA = false;
 }
