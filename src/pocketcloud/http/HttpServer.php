ba9b472ed0d42d4f2eb6560e57122ec1dab2bc30
r3pt1s <leonistmegacool2@gmail.com>
2024-04-24 18:27:01 +0200

    added @ to socket_accept

--- src/pocketcloud/http/HttpServer.php
+++ src/pocketcloud/http/HttpServer.php
@@ -107,7 +107,7 @@ class HttpServer extends Thread implements Reloadable {
 
     public function accept(): ?SocketClient {
         if (!$this->connected) return null;
-        if (($c = socket_accept($this->socket)) !== false && $c instanceof Socket) return SocketClient::fromSocket($c);
+        if (($c = @socket_accept($this->socket)) !== false && $c instanceof Socket) return SocketClient::fromSocket($c);
         return null;
     }
 
