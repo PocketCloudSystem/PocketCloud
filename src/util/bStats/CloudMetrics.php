bd8f5320e1c7769a1368983782275eb7cfb164ed
r3pt1s <leonistmegacool2@gmail.com>
2026-01-24 16:44:48 +0100

    added bStats charts

--- src/util/bStats/CloudMetrics.php
+++ src/util/bStats/CloudMetrics.php
@@ -3,9 +3,13 @@
 namespace pocketcloud\cloud\util\bStats;
 
 use pocketcloud\cloud\config\impl\MainConfig;
+use pocketcloud\cloud\player\CloudPlayerManager;
+use pocketcloud\cloud\server\CloudServerManager;
 use pocketcloud\cloud\util\misc\Tickable;
 use pocketcloud\cloud\util\VersionInfo;
 use Ramsey\Uuid\UuidInterface;
+use xxFLORII\bStats\chart\def\SimplePieChart;
+use xxFLORII\bStats\chart\def\SingleLineChart;
 use xxFLORII\bStats\Metrics;
 use xxFLORII\bStats\settings\MetricsSettings;
 
@@ -22,6 +26,10 @@ final class CloudMetrics implements Tickable {
             $this->mainConfig->isBStatsLogFailedRequests(), $this->mainConfig->isBStatsLogSentData(),
             $this->mainConfig->isBStatsLogStatusResponseText(), $this->uuid->toString()
         ));
+
+        $this->metrics->addChart(new SingleLineChart("players", fn(): int => count(CloudPlayerManager::getInstance()->getAll())));
+        $this->metrics->addChart(new SingleLineChart("managed_servers", fn(): int => count(CloudServerManager::getInstance()->getAll())));
+        $this->metrics->addChart(new SimplePieChart("version", fn(): string => VersionInfo::VERSION));
     }
 
     public function tick(int $currentTick): void {
